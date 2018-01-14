e8ce4819509bfb0a4f279753530c8b3a435ac1b5
bennybop <bennybop74@yahoo.com>
2018-01-14 23:11:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,32 @@
 require 'pry'
 
-class Artist 
-  attr_accessor :name 
-  attr_reader :songs 
+class Artist
+  attr_accessor :name
+  attr_reader :songs
 
-  extend Memorable::ClassMethods 
-  extend Findable::ClassMethods 
-  include Memorable::InstanceMethods 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
   include Paramable::InstanceMethods
 
   @@artists = []
 
-  def self.all 
-    @@artists 
-  end 
+  def self.all
+    @@artists
+  end
 
-  def initialize 
-    super 
+  def initialize
+    super
     @songs = []
-  end 
+  end
 
   def add_song(song)
-    @songs << song 
-    song.artist = self unless song.artist 
-  end 
+    @songs << song
+    song.artist = self unless song.artist
+  end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end 
+  end
 
 end 
